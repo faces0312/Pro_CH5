34d05b83433932855af4f1bd77c9b082626c5b62
HyunSeok Suh <suhya990312@gmail.com>
2024-11-08 23:33:47 +0900

    Stat_Init

---
+++ Assets/Script/ScriptableObject/Stat.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "Stat", menuName = "New Stem")]
+public class Stat : ScriptableObject
+{
+    
+}
