3bcaddb92ec05895fdc420e1a25c78f47c146665
HyunSeok Suh <suhya990312@gmail.com>
2024-11-13 16:30:53 +0900

    TouchFix

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
@@ -9,14 +10,19 @@ public class PlayerController : MonoBehaviour
     public void OnClick(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started)
-        {
+        {   
+            //게임오브젝트에 터치했는지 확인
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                Debug.Log("터치 불가");
+                return;
+            }
             // 터치 위치 가져오기
             Vector3 touchPosition = Input.mousePosition;
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(touchPosition.x, touchPosition.y, Camera.main.nearClipPlane));
             if(GameManager.Instance.Is_Battle == false)
             {
                 Data.Instance.gameData.money += (Data.Instance.gameData.touchLV + 1);
-                //Effect
                 GameManager.Instance.Player.particle.CreateMainParticle(worldPosition);
                 Debug.Log("터치 발동");
             }
